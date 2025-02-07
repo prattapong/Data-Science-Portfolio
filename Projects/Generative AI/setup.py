630509d7243219d8d37fb6dec8ea54ec3aaa7054
rattapongpoj <bl.rattapong@hotmail.com>
2025-02-07 19:40:32 +0700

    add folders and files

---
+++ Projects/Generative AI/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    auther = 'Rattapong Pojpatinya',
+    author_email = 'bl.rattapong@hotmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
